145d6edc314769b5dbfcd2f4c9c2b6755f42f244
Dejan Stosic <dejan7stosic@gmail.com>
2019-11-14 14:52:51 +0100

    select fix

--- resources/client/main.js
+++ resources/client/main.js
@@ -3,6 +3,7 @@ import ArgonTheme from './argon/plugins/dashboard-plugin';
 import store from './store'
 import router from './router/router';
 import App from './App.vue';
+import {Select, Option} from 'element-ui'
 
 window._ = require('lodash');
 window.axios = require('axios');
@@ -10,6 +11,8 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 Vue.use(ArgonTheme);
+Vue.use(Select);
+Vue.use(Option);
 
 /* eslint-disable no-new */
 new Vue({
