b949da7f998bc7081380f4d1e18f79ede62a8057
veljko radenkovic <veljko.radenkovic@planetsg.com>
2019-11-15 23:35:16 +0100

    user api resources

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\User;
 use App\Repositories\Users\UsersRepo;
 use App\Http\Resources\User as UserResource;
 
@@ -14,6 +15,15 @@ class UsersController extends Controller
         $this->usersRepo = $usersRepo;
     }
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        return UserResource::collection(\App\Models\User::all());
+    }
+
     public function read($id)
     {
         if ($id === "me") {
@@ -24,4 +34,67 @@ class UsersController extends Controller
             return new UserResource($this->usersRepo->find($id));
         }
     }
+
+
+    public function create(\Request $request){
+
+        $this->validate($request, [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+//            'image' => 'required|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        $user = new \App\Models\User();
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $name = str_slug($request->title) . '.' . $image->getClientOriginalExtension();
+            $destinationPath = public_path('/images/user_media');
+            $imagePath = $destinationPath . "/" . $name;
+            $image->move($destinationPath, $name);
+            $user->avatar = $name;
+        }
+
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->avatar = $request->avatar;
+        $user->save();
+
+        return new UserResource($user);
+
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(\Request $request, \App\Models\User $user)
+    {
+        $this->validate($request, [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+        ]);
+
+        $user->update($request->only(['first_name', 'last_name', 'email']));
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+
+    public function delete(\App\Models\User $user)
+    {
+        $user->delete();
+        return response()->json(null, 204);
+    }
+
 }
