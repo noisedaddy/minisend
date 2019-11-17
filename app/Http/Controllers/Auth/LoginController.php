9a27ebd31fa2540208b60d8f565b43dc46b1b5bd
veljko radenkovic <veljko.radenkovic@planetsg.com>
2019-11-17 16:33:20 +0100

    test user db seed and login header added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,6 +28,6 @@ class LoginController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
+        ])->header('Authorization', $token);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => bcrypt('123123')
         ]);
-
         factory(\App\Models\User::class, 100)->create()->each(function ($user) {
             $user->save();
         });
