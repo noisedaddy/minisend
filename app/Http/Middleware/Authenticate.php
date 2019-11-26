7662ca19bd7b9d96987cccc510df26ed0a0ffa02
veljko radenkovic <veljko.radenkovic@planetsg.com>
2019-11-26 21:31:49 +0100

    initial

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,6 +6,12 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+
+    public function authenticate($request, array $guards)
+    {
+        return true;
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
