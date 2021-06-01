b022db41bf13d6c39ed45914b7ef245113467196
veljko <veljko.radenkovic@gmail.com>
2021-06-01 12:33:07 +0200

    another test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,13 +4,17 @@ namespace Tests\Feature;
 
 use App\Jobs\ExampleJob;
 use App\Models\User;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Mail\Events\MessageSending;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, DatabaseMigrations;
 
     /**
      * A basic test example.
@@ -50,5 +54,15 @@ class ExampleTest extends TestCase
                     ]
                 ]
             );
+    }
+
+    public function testEvent(){
+
+        Event::fake();
+        Event::dispatch(Login::class);
+        Event::assertDispatched(Login::class,function ($e) {
+            return ($e);
+        });
+
     }
 }
